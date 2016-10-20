1782e6924b51daf3f869616af995f060d96ee688
Alexu039 <xfl039@163.com>
2016-10-20 21:34:29 +0800

    test001
    test001

---
+++ test001.py
@@ -0,0 +1,26 @@
+import random
+
+print ("欢迎来到xfl猜数字世界！")
+secret = random.randint(1,10)
+loop_cnt = 3
+temp = input("请猜测一个1-10的数字：")
+guess = int(temp)
+if guess == secret:
+    print ("你猜对了，真棒！")
+elif guess > secret:
+    print ("你猜的有点大了！")
+else:
+    print ("你猜的有点小了！")
+while guess != secret and loop_cnt > 1:
+    temp = input("请重新猜测一个1-10的数字：")
+    guess = int(temp)
+    if guess == secret:
+        print ("你猜对了，真棒！")
+    elif guess > secret:
+        print ("你猜的有点大了！")
+    else:
+        print ("你猜的有点小了！")
+
+    loop_cnt = loop_cnt - 1
+
+print ("游戏结束！")
